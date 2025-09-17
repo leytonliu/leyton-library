561bfb34872908e2a0670532e12b61ef83b32392
liulidong <lidong.liu@hand-china.com>
2025-09-17 14:44:24 +0800

    test: 引入 Vitest 与用例；调整 tsconfig 排除测试；完善目录 README

---
+++ types/promise-queue/index.d.ts
@@ -0,0 +1,12 @@
+declare class PromiseQueue {
+    queue: (() => Promise<any>)[];
+    limit: number;
+    result: any[];
+    current: number;
+    running: number;
+    constructor(queue: (() => Promise<any>)[], limit: number);
+    runNext(): void;
+    run(): void;
+}
+export { PromiseQueue };
+export default PromiseQueue;
