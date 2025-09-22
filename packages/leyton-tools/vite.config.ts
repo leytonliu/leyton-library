90eb24cbf10f0de2ceba2a0c1fa2405962582f58
liulidong <lidong.liu@hand-china.com>
2025-09-22 11:08:34 +0800

    feat:1

--- packages/leyton-tools/vite.config.ts
+++ packages/leyton-tools/vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'lib/index.ts'),
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'LeytonTools',
       fileName: 'index'
     },
