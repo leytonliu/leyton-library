bc57c8f6444793248baa82bd7cfd6f20b1db67b5
liulidong <lidong.liu@hand-china.com>
2025-09-22 10:52:51 +0800

    feat: 11

---
+++ scripts/version-update.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+
+const versionType = process.argv[2] || 'patch';
+
+// 更新子包版本
+const packagesDir = path.join(__dirname, '..', 'packages');
+const packages = fs.readdirSync(packagesDir);
+
+packages.forEach(pkg => {
+  const pkgPath = path.join(packagesDir, pkg, 'package.json');
+  if (fs.existsSync(pkgPath)) {
+    const pkgJson = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
+    const currentVersion = pkgJson.version;
+    const [major, minor, patch] = currentVersion.split('.').map(Number);
+    
+    let newVersion;
+    switch (versionType) {
+      case 'major':
+        newVersion = `${major + 1}.0.0`;
+        break;
+      case 'minor':
+        newVersion = `${major}.${minor + 1}.0`;
+        break;
+      case 'patch':
+      default:
+        newVersion = `${major}.${minor}.${patch + 1}`;
+        break;
+    }
+    
+    pkgJson.version = newVersion;
+    fs.writeFileSync(pkgPath, JSON.stringify(pkgJson, null, 2) + '\n');
+    console.log(`Updated ${pkg} from ${currentVersion} to ${newVersion}`);
+  }
+});
+
+// 更新主包版本
+const rootPkgPath = path.join(__dirname, '..', 'package.json');
+const rootPkgJson = JSON.parse(fs.readFileSync(rootPkgPath, 'utf8'));
+const currentVersion = rootPkgJson.version;
+const [major, minor, patch] = currentVersion.split('.').map(Number);
+
+let newVersion;
+switch (versionType) {
+  case 'major':
+    newVersion = `${major + 1}.0.0`;
+    break;
+  case 'minor':
+    newVersion = `${major}.${minor + 1}.0`;
+    break;
+  case 'patch':
+  default:
+    newVersion = `${major}.${minor}.${patch + 1}`;
+    break;
+}
+
+rootPkgJson.version = newVersion;
+fs.writeFileSync(rootPkgPath, JSON.stringify(rootPkgJson, null, 2) + '\n');
+console.log(`Updated root package from ${currentVersion} to ${newVersion}`);
+
+console.log('Version update completed!');
